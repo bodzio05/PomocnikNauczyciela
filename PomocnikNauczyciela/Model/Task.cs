93a4d7221f1395e9eb58239227d4f7782d1740cf
Marcin Cierlik <marcin.cierlik@gmail.com>
2019-11-30 13:45:08 +0100

    Add project files.

---
+++ PomocnikNauczyciela/Model/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomocnikNauczyciela.Model
+{
+    public class Task
+    {
+        #region Proprties
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Hashtag> Hashtags { get; set; }
+        public TaskType Type { get; set; }
+        #endregion
+
+        #region Constructor
+        public Task(TaskType type, string name)
+        {
+            this.Type = type;
+            this.Name = name;
+            Hashtags = new List<Hashtag>();
+        }
+
+        public Task(TaskType type, string name, string description): this(type,name)
+        {
+            this.Description = description;
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ PomocnikNauczyciela/View/TaskViewModel.cs
@@ -0,0 +1,34 @@
+﻿using PomocnikNauczyciela.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PomocnikNauczyciela.View
+{
+    public class TaskViewModel: BaseViewModel
+    {
+        #region Properties
+        private int myProperty;
+        public int MyProperty
+        {
+            get { return myProperty; }
+            set
+            {
+                SetProperty(ref myProperty, value);
+            }
+        }
+        #endregion
+
+            #region Constructor
+        public TaskViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
